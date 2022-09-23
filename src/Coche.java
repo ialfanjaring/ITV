3edc82ae97f9cd858b9e6befaabf6ed8be5790d6
ivan <ialfanjaring@iesch.org>
2022-09-23 21:15:56 +0200

    Initial commit

---
+++ src/Coche.java
@@ -0,0 +1,70 @@
+import java.util.Date;
+
+public class Coches {
+    private String matricula;
+    private String modelo;
+    private String marca;
+    private String nombreTitular;
+    private String DNI;
+    private Date fechaCaducidad;
+    private String problemas;
+
+
+
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getNombreTitular() {
+        return nombreTitular;
+    }
+
+    public void setNombreTitular(String nombreTitular) {
+        this.nombreTitular = nombreTitular;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+
+    public Date getFechaCaducidad() {
+        return fechaCaducidad;
+    }
+
+    public void setFechaCaducidad(Date fechaCaducidad) {
+        this.fechaCaducidad = fechaCaducidad;
+    }
+
+    public String getProblemas() {
+        return problemas;
+    }
+
+    public void setProblemas(String problemas) {
+        this.problemas = problemas;
+    }
+}
